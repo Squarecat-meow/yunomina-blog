0ffd5e3b273754f168919041be8b6a53ab1f0e6f
Squarecat-meow <yozumina@serafuku.moe>
2024-11-21 11:14:19 +0900

    🗃️ DB Init 마이그레이션 파일 생성

---
+++ prisma/migrations/20241121020213_init/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "user" (
+    "id" SERIAL NOT NULL,
+    "userId" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "salt" TEXT NOT NULL,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_userId_key" ON "user"("userId");
