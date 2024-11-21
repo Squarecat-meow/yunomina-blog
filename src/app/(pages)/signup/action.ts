46dda06764cf248eba840a9ced3e99c985838eab
Squarecat-meow <yozumina@serafuku.moe>
2024-11-21 18:57:51 +0900

    🚧 회원가입 Server action 작성

---
+++ src/app/(pages)/signup/action.ts
@@ -0,0 +1,28 @@
+"use server";
+
+import { signupDto } from "@/app/_dto/signup.dto";
+import bcrypt from "bcrypt";
+
+async function hashPassword(password: string): Promise<string> {
+  const saltRound = 15;
+
+  return new Promise<string>((res) => {
+    bcrypt.hash(password, saltRound, async (err, hash) => {
+      if (err) {
+        console.error("패스워드 해싱 중 에러: " + err);
+      }
+      res(hash);
+    });
+  });
+}
+
+export async function postSignup({ invitationCode, id, password }: signupDto) {
+  const hashedPasswd = await hashPassword(password);
+  const payloadWithHashedPassword: signupDto = {
+    id: id,
+    password: hashedPasswd,
+    invitationCode: invitationCode,
+  };
+
+  return payloadWithHashedPassword;
+}
