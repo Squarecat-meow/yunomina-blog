b0c7be4459a7283233e642636f15b52d2e47ec10
Squarecat-meow <yozumina@serafuku.moe>
2024-12-03 10:31:42 +0900

    💄 포스트 페이지 스타일 수정

--- src/app/(pages)/posts/[slug]/page.tsx
+++ src/app/(pages)/posts/[slug]/page.tsx
@@ -61,42 +61,42 @@ export default async function Post({ params }: PageProps) {
   });
 
   return (
-    <div>
-      <div className="prose flex justify-center p-4">
-        <div className="w-full desktop:w-[80%]">
-          <h1 className="text-5xl mb-2">{frontmatter.title}</h1>
+    <div className="w-full desktop:w-[80%] prose">
+      <div className="flex flex-col justify-center p-4">
+        <h1 className="text-5xl mb-2">{frontmatter.title}</h1>
+        <div className="flex items-center mb-2">
           <span className="text-sm">
             {frontmatter.postDate.toLocaleString("ko-KR", {
               timeZone: "Asia/Seoul",
             })}
           </span>
           <span className="ml-4">{frontmatter.category}</span>
-          <div className="h-fit flex items-center gap-2">
-            {user.avatarUrl ? (
-              <Image
-                src={user.avatarUrl}
-                width={50}
-                height={50}
-                alt="author avatar"
-                className="m-0 rounded-full"
-              />
-            ) : (
-              <div className="skeleton w-[50px] h-[50px] rounded-full" />
-            )}
-            <span className="text-lg">{frontmatter.author}</span>
-          </div>
-          <div className="text-lg">{content}</div>
         </div>
+        <div className="h-fit flex items-center gap-2">
+          {user.avatarUrl ? (
+            <Image
+              src={user.avatarUrl}
+              width={50}
+              height={50}
+              alt="author avatar"
+              className="m-0 rounded-full"
+            />
+          ) : (
+            <div className="skeleton w-[50px] h-[50px] rounded-full" />
+          )}
+          <span className="text-lg">{frontmatter.author}</span>
+        </div>
+        <div className="text-lg">{content}</div>
       </div>
       <div className="w-full flex justify-between">
         {prevPost[0] ? (
-          <div className="flex flex-col rounded-box px-6 py-2 hover:shadow transition-shadow">
-            <Link href={`/posts/${prevPost[0].id}`}>
+          <div className="flex flex-col rounded-box px-4 py-2 hover:shadow transition-shadow">
+            <Link href={`/posts/${prevPost[0].id}`} className="no-underline">
+              <span className="text-sm w-full flex justify-end">이전 글</span>
               <div className="flex items-center gap-2">
                 <ChevronLeft size={24} />
-                <span className="text-xl">이전 글</span>
+                <span className="text-2xl font-bold">{prevPost[0].title}</span>
               </div>
-              <span className="text-2xl font-bold">{prevPost[0].title}</span>
             </Link>
           </div>
         ) : (
@@ -104,12 +104,12 @@ export default async function Post({ params }: PageProps) {
         )}
         {nextPost[0] && (
           <div className="flex flex-col rounded-box px-6 py-2 hover:shadow transition-shadow">
-            <Link href={`/posts/${nextPost[0].id}`}>
+            <Link href={`/posts/${nextPost[0].id}`} className="no-underline">
+              <span className="text-sm w-full flex">다음 글</span>
               <div className="flex items-center gap-2">
-                <span className="text-xl">다음 글</span>
+                <span className="text-2xl font-bold">{nextPost[0].title}</span>
                 <ChevronRight size={24} />
               </div>
-              <span className="text-2xl font-bold">{nextPost[0].title}</span>
             </Link>
           </div>
         )}
