d070b018646bebd22d420cbe4d34b85228690987
Squarecat-meow <yozumina@serafuku.moe>
2024-12-19 16:24:39 +0900

    🚧 WIP

--- src/app/api/users/blog/inbox/route.ts
+++ src/app/api/users/blog/inbox/route.ts
@@ -1,7 +1,8 @@
 import { sendApiError } from "@/utils/apiHandler/sendApiError";
 import { NextRequest, NextResponse } from "next/server";
 import { AP } from "activitypub-core-types";
-import { verify } from "crypto";
+import { parse, ParseResponse, verifySignature } from "http-signature";
+import { ClientRequest } from "http";
 
 async function fetchActorInformation(
   actorUrl: string
@@ -22,40 +23,47 @@ async function fetchActorInformation(
   return null;
 }
 
-function parseSignature(signature: string) {
-  const parseRegex = /"[^"]*"/g;
-  const parsedSignature = signature.match(parseRegex);
-  if (!parsedSignature) return;
+function verify(signature: ParseResponse, publicKeyJson: AP.Actor) {
+  let signatureValid;
 
-  const signatureObject = {
-    keyId: parsedSignature[0].replaceAll('"', ""),
-    algorithm: parsedSignature[1].replaceAll('"', ""),
-    signature: parsedSignature[3].replaceAll('"', ""),
-  };
+  try {
+    // Verify the signature
+    signatureValid = verifySignature(
+      signature,
+      publicKeyJson.publicKey!.publicKeyPem
+    );
+  } catch (error) {
+    console.log("Signature Verification error", error);
+  }
 
-  return signatureObject;
+  return signatureValid;
 }
 
 export async function POST(req: NextRequest) {
-  const data = await req.json();
-  const dataBuffer = Buffer.from(JSON.stringify(data));
-  const signature = req.headers.get("signature");
-  if (!signature) return sendApiError(401, "HTTP 시그니쳐가 없어요!");
-  const signatureObject = parseSignature(signature);
-  if (!signatureObject)
-    return sendApiError(400, "HTTP 시그니쳐가 맞지 않아요!");
+  const a = new Map<string, string>([]);
+  const h = req.headers.entries();
+  for (const e of h) {
+    const [k, v] = e;
+    a.set(k, v);
+  }
 
-  const actorInformation = await fetchActorInformation(signatureObject.keyId);
+  const he = Object.fromEntries(a);
+  const newHeader = {} as ClientRequest;
+  Object.assign(newHeader, { headers: he });
+  Object.assign(newHeader, { method: req.method });
+  Object.assign(newHeader, { url: req.url });
+  Object.assign(newHeader, { httpVersion: "HTTP/2.0" });
+
+  const signature = parse(newHeader);
+  if (!signature) return sendApiError(401, "HTTP 시그니쳐가 없어요!");
+  // const signatureObject = parseSignature(signature);
+  // if (!signatureObject)
+  //   return sendApiError(400, "HTTP 시그니쳐가 맞지 않아요!");
+
+  const actorInformation = await fetchActorInformation(signature.params.keyId);
   if (!actorInformation || !actorInformation.publicKey)
     return sendApiError(401, "액터 정보를 가져올 수 없어요!");
-  const keyBuffer = Buffer.from(signatureObject.signature);
-
-  const isVerified = verify(
-    signatureObject.algorithm,
-    dataBuffer,
-    actorInformation.publicKey.publicKeyPem,
-    keyBuffer
-  );
+  const isVerified = verify(signature, actorInformation);
 
   if (isVerified === false || typeof isVerified !== "boolean") {
     console.log("검증에 실패했어요! ", isVerified);
